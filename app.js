8babeb11e4dfdfa24b2c828059eab5b155aaf6d9
lochloch <lochana.ahire17@gmail.com>
2020-09-04 15:24:24 -0500

    apply bootstrap to app

--- app.js
+++ app.js
@@ -4,21 +4,30 @@ app.controller("firstController", function($scope){
 $scope.message = 'To Do App';
 // Add product to list
 $scope.product = { title: "", desc: ""};
-$scope.products = [];
+$scope.products = [
+        {title: 'Milk', desc: 'white'},
+        {title: 'Coco', desc: 'Brown'}
+];
 $scope.cur = { title: "", desc: ""};
+$scope.showCur = false;
 $scope.addItem = function () {
     $scope.products.push($scope.product);
     $scope.product = { title: "", desc: ""};
+    $scope.showCur = false;
 } 
 // Remove product from list
 $scope.removeItem = function (x) {
-    $scope.products.splice(x, 1);
-    
+    $scope.products.splice(x, 1); 
 }  
 
 // Display particular product-description
 $scope.displayItem = function (x) {
     $scope.cur = $scope.products[x];
+    $scope.showCur = true;
 }  
+// back button result
+$scope.backDisp = function() {
+ $scope.showCur = false;
+} 
 
 });
